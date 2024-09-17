ee35c48847d8eaf695acff9f5aa5e1459fec6154
asfound <fffound27@gmail.com>
2024-09-17 12:12:35 +0200

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,28 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.candidate = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
 module.exports = GuessingGame;
+
+
+
+
+
+// 409 with max value 4048
+// AssertionError: expected 407 to equal 409
